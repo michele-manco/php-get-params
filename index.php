4f5a08bffe2a75ead2acf1cd3db8a35b418df716
Michele_Manco <motlamik@gmail.com>
2020-01-20 20:19:28 +0100

    slightly mod

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $badword = $_GET['badword'];
 $frase = "Una mattina mi son svegliato oh bella ciao bella ciao bella ciao ciao ciao una mattina mi son svegliato e ho trovato invasor";
-$frase_sostituita = str_replace('invasor', '***', $frase);
+$frase_sostituita = str_replace($badword, '***', $frase);
 echo $frase_sostituita
 
 ?>
-<!-- <h1>Una mattina mi son svegliato oh bella ciao bella ciao bella ciao ciao ciao una mattina mi son svegliato e ho trovato invasor <?php echo $nome ?> </h1> -->
+<!-- <h1>Una mattina mi son svegliato oh bella ciao bella ciao bella ciao ciao ciao una mattina mi son svegliato e ho trovato  <?php echo $badword ?> </h1> -->
 <!-- <pre>
-  <?php print_r($frase); ?>
+  <?php print_r($frase_sostituita); ?>
 </pre> -->
 
 <?php
